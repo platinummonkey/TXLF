58bbf9a5a2c9c4d1012a26ed712369b247e78fbd
platinummonkey <platinummonkey@archlinux.us>
2013-04-22 11:38:16 -0500

    some css fixes, need to fix animationend problem

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
-var theScroll;
+/*var theScroll;
 function scroll(){
     theScroll = new iScroll('wrapper');
 }
 document.addEventListener('DOMContentLoaded', scroll, false);
-
+*/
 //function page(toPage) {
 	//var toPage = $(toPage),
 	//fromPage = $("#pages .current");
@@ -18,18 +18,34 @@ document.addEventListener('DOMContentLoaded', scroll, false);
 //}
 
 $('#tab-bar a').on('click', function(e){
+        console.log('button clicked');
 	e.preventDefault();
-	var nextPage = $(e.target.hash);
-  //var toPage = $(e.target.hash);
-	$("#pages .current").removeClass("current");
-	nextPage.addClass("current");
-  //fromPage = $("#pages .current");
-	//if(toPage.hasClass("current") || toPage === fromPage) {
-		//return;
-	//};
-	//toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
-		//fromPage.removeClass("current fade out");
-		//toPage.removeClass("fade in")
-	//});
+        var toPage = $(e.target.hash);
+        console.log(toPage);
+	//$("#pages .current").removeClass("current");
+        fromPage = $("#pages .current");
+        console.log(fromPage);
+	if(toPage.hasClass("current") || toPage === fromPage) {
+		return;
+	};
+	toPage.addClass("current fade in").one("animationend", function() { //.one("webkitAnimationEnd", function(){
+                console.log('animation end');
+		/*//fromPage.removeClass("current fade out");
+                fromPage.removeClass();
+                console.log('removed classes');
+		toPage.removeClass("fade in");
+                console.log('removed fade in');
+                //toPage.removeClass();
+                //toPage.addClass("current");*/
+	});
 	//fromPage.addClass("fade out");
+        console.log('finish');
 });
+
+
+// scrolling
+var theScroll;
+function scroll(){
+    theScroll = new iScroll('wrapper');
+}
+document.addEventListener('DOMContentLoaded', scroll, false);
