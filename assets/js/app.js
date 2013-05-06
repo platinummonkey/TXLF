3d26fbd3c5f5f5f978143ae6d675964f35f8dc19
Cody Lee <platinummonkey@archlinux.us>
2013-05-05 23:09:46 -0500

    nightly checkin

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,6 +76,7 @@ function addClassNameListener(elemId, callback) {
 
 addClassNameListener("scan_qrcode", function(){ scanCode(); });
 
+/* Sponsors */
 function getSponsors() {
 	console.log("Getting sponsors");
 	surl = "http://2013.texaslinuxfest.org/sponsors_mobile";
@@ -95,8 +96,9 @@ function getSponsors() {
 			  sponsor = '<li class="' + node.node.field_sponsorship_level + '">'
 					    + '<a class="avatar" href="' + node.node.field_sponsor_link + '"><img src="' + node.node.field_sponsor_logo + '" alt="' + node.node.title + '" /></a>'
 					    + '<a href="' + node.node.field_sponsor_link + '">' + node.node.title + '</a><br />'
-					    + '<span class="sponsorlevel">' + node.node.field_sponsorship_level + ' Sponsor</span></li>';
+					    + '<span class="sponsorlevel">' + node.node.field_sponsorship_level + ' Sponsor - ' + node.node.body + '...</span></li>';
 			  $("#sponsors-list").append(sponsor);
+			  theScroll.refresh();
 		  });
         },
         error: function(e) {
@@ -107,3 +109,39 @@ function getSponsors() {
 };
 
 addClassNameListener("sponsors-content", function(){ getSponsors(); });
+/* End Sponsors */
+
+/* Sessions */
+function getSessions() {
+	console.log("Getting sponsors");
+	surl = "http://2013.texaslinuxfest.org/sponsors_mobile";
+    $.ajax({
+       type: 'GET',
+        url: surl,
+        async: false,
+        jsonpCallback: 'sponsors',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: function(data) {
+          console.log("clearing DOM");
+		  $("#sponsors-list").empty();
+		  console.log("filling in sponsors");
+		  $.each(data.nodes, function (i, node) {
+			  //console.log(node.node);
+			  sponsor = '<li class="' + node.node.field_sponsorship_level + '">'
+					    + '<a class="avatar" href="' + node.node.field_sponsor_link + '"><img src="' + node.node.field_sponsor_logo + '" alt="' + node.node.title + '" /></a>'
+					    + '<a href="' + node.node.field_sponsor_link + '">' + node.node.title + '</a><br />'
+					    + '<span class="sponsorlevel">' + node.node.field_sponsorship_level + ' Sponsor - ' + node.node.body + '...</span></li>';
+			  $("#sponsors-list").append(sponsor);
+			  theScroll.refresh();
+		  });
+        },
+        error: function(e) {
+           console.log(e.message);
+        }
+     });
+	console.log('done sponsors');
+};
+
+addClassNameListener("program-content", function(){ getSessions(); });
+/* End Sessions */
